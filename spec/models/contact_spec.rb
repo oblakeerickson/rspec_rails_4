64582e384beed8a2ab98026a1cc034f62a844833
Blake Erickson <o.blakeerickson@gmail.com>
2013-12-18 07:54:23 -0700

    added tests to check phone numbers and sorting contacts

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -16,7 +16,47 @@ describe Contact do
   it "is invalid without a lastname" do
     expect(Contact.new(lastname: nil)).to have(1).errors_on(:lastname)
   end
-  it "is invalid without an email address"
-  it "is invalid with a duplicate email address"
-  it "returns a contact's full name as a string"
+
+  it "is invalid without an email address" do
+    expect(Contact.new(email: nil)).to have(1).errors_on(:email)
+  end
+
+  it "is invalid with a duplicate email address" do
+    Contact.create(
+      firstname: 'Joe', lastname: 'Tester',
+        email: 'tester@example.com')
+    contact = Contact.new(
+      firstname: 'Jane', lastname: 'Tester',
+        email: 'tester@example.com')
+    expect(contact).to have(1).errors_on(:email)
+  end
+
+  it "returns a contact's full name as a string" do
+    contact = Contact.new(firstname: 'John', lastname: 'Doe',
+      email: 'johndoe@example.com')
+    expect(contact.name).to eq 'John Doe'
+  end
+
+  describe "filter last name by letter" do
+    before :each do
+      @smith = Contact.create(firstname: 'John', lastname: 'Smith',
+        email: 'jsmith@example.com')
+      @jones = Contact.create(firstname: 'Tim', lastname: 'Jones',
+        email: 'tjones@example.com')
+      @johnson = Contact.create(firstname: 'John', lastname: 'Johnson',
+        email: 'jjohnson@example.com')
+    end
+
+    context "matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("J")).to eq [@johnson, @jones]
+      end
+    end
+
+    context "non-matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("J")).to_not include @smith
+      end
+    end
+  end
 end
