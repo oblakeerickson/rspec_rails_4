b31429c4a4596dba05370dae03d30be1ea1930a4
Blake Erickson <o.blakeerickson@gmail.com>
2013-12-19 08:02:12 -0700

    fixed filter last name by letter test data

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -37,26 +37,26 @@ describe Contact do
     expect(create(:contact).phones.count).to eq 3
   end
 
-  # describe "filter last name by letter" do
-  #   before :each do
-  #     @smith = Contact.create(firstname: 'John', lastname: 'Smith',
-  #       email: 'jsmith@example.com')
-  #     @jones = Contact.create(firstname: 'Tim', lastname: 'Jones',
-  #       email: 'tjones@example.com')
-  #     @johnson = Contact.create(firstname: 'John', lastname: 'Johnson',
-  #       email: 'jjohnson@example.com')
-  #   end
+  describe "filter last name by letter" do
+    before :each do
+      @smith = create(:contact,
+        lastname: 'Smith', email: 'jsmith@example.com')
+      @jones = create(:contact,
+        lastname: 'Jones', email: 'tjones@example.com')
+      @johnson = create(:contact,
+        lastname: 'Johnson', email: 'jjohnson@example.com')
+    end
 
-  #   context "matching letters" do
-  #     it "returns a sorted array of results that match" do
-  #       expect(Contact.by_letter("J")).to eq [@johnson, @jones]
-  #     end
-  #   end
+    context "matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("J")).to eq [@johnson, @jones]
+      end
+    end
 
-  #   context "non-matching letters" do
-  #     it "returns a sorted array of results that match" do
-  #       expect(Contact.by_letter("J")).to_not include @smith
-  #     end
-  #   end
-  # end
+    context "non-matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("J")).to_not include @smith
+      end
+    end
+  end
 end
